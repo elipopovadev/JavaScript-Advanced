52a03ae9039d00f5f5d531c83571ce8c2f4db5ea
Elena Popova <elipopovadev@gmail.com>
2021-07-31 22:05:05 +0200

    LargestNumber

---
+++ SyntaxFunctionsAndStatements-Lab/LargestNumber.js
@@ -0,0 +1,6 @@
+let findLargestNumber = (arg1, arg2, arg3) => {
+    let largestNumber = Math.max(arg1, arg2, arg3);
+    console.log(`The largest number is ${largestNumber}.`);
+}
+
+findLargestNumber(5, -3, 16);
