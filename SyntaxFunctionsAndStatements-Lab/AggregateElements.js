72558c93c17c48f6d38a64d468b6c0261afa6e27
Elena Popova <elipopovadev@gmail.com>
2021-08-02 18:08:24 +0200

    AggregateElements

--- SyntaxFunctionsAndStatements-Lab/AggregateElements.js
+++ SyntaxFunctionsAndStatements-Lab/AggregateElements.js
@@ -1,12 +1,17 @@
 let aggregateElements = (...array) => {
     let numbersArray = array.map(Number);
     let sum = numbersArray.reduce((a, b) => a + b);
-    let inverseValue = numbersArray.reduce((a, b) => 1/a + 1/b);
+ 
+    let inverseValuesSum = 0;
+    for (let i = 0; i < numbersArray.length; i++) {
+        inverseValuesSum += 1 / numbersArray[i];
+    }
+ 
     let stringConcat = numbersArray.join('');
-
     console.log(sum);
-    console.log(inverseValue.toFixed(4));
+    console.log(inverseValuesSum);
     console.log(stringConcat);
 }
+ 
 
 aggregateElements(2, 4, 8, 16);
