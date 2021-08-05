0a3689249bb8ce9c3887a6ed4ecd1d596be8a633
Elena Popova <elipopovadev@gmail.com>
2021-08-05 22:46:11 +0200

    SameNumbers

--- SyntaxFunctionsAndStatements-Exercise/SameNumbers.js
+++ SyntaxFunctionsAndStatements-Exercise/SameNumbers.js
@@ -3,16 +3,17 @@ function checkForTheSameDigits(input){
     let areTheSame = true;
     let sum = 0;
 
-    let firstDigit = input[0];
-    for(let i = 0; i < input.length; i++ ){
-     if(input[i] !== firstDigit) {
+    for(let i = 0; i < input.length - 1; i++ ){
+     if(input[i] !== input[i+1]){
          areTheSame = false;       
-     }
+     }    
+
      sum += Number(input[i]);
     }
 
+    sum += Number(input[input.length-1]);
     console.log(areTheSame);
     console.log(sum);
 }
 
-checkForTheSameDigits(2222)
+checkForTheSameDigits(1234)
