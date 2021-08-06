c535353b22b664d17ab327487a68c97b8bdfeb92
Elena Popova <elipopovadev@gmail.com>
2021-08-06 22:10:56 +0200

    ValidityChecker

---
+++ SyntaxFunctionsAndStatements-Exercise/ValidityChecker.js
@@ -0,0 +1,24 @@
+function validityChecker ([x1, y1, x2, y2]){
+    let firstDistance = Math.sqrt((0 - x1) ** 2 + (0 - y1) ** 2);
+    if(Number.isInteger(firstDistance)){
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+    }
+
+    let secondDistance = Math.sqrt((0 - x2) ** 2 + (0 - y2) ** 2);
+    if(Number.isInteger(secondDistance)){
+        console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+    }
+
+    let thirdDistance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+    if(Number.isInteger(thirdDistance)){
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    } else {
+        console.log(`{${x1}, ${{y1}} to {${x2}, ${y2}} is invalid`);
+    }
+}
+
+validityChecker([2, 1, 1, 1]);
