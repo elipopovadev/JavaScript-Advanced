7c414ea3a22c45349070f502a97f84179657ec82
Elena Popova <elipopovadev@gmail.com>
2021-08-09 22:51:08 +0200

    WordsUppercase-RegEx

--- SyntaxFunctionsAndStatements-Exercise/WordsUppercase.js
+++ SyntaxFunctionsAndStatements-Exercise/WordsUppercase.js
@@ -1,6 +1,6 @@
 function solve(text) {
     let result = text.toUpperCase()
-      .match(/\w+/g) // \w+ and g to match every word; / .... / is for regEx
+      .match(/\w+/g) // \w+ and g to match every word; / .... / is for regEx; i is for capital insensitive; /[^9]/ - match everything except 9
       .join(', ');
       
     console.log(result);
