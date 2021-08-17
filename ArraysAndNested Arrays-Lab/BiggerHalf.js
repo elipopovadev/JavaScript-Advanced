e997a61c931346d0f74290088571494d7709df27
Elena Popova <elipopovadev@gmail.com>
2021-08-17 15:28:42 +0200

    BiggerHalf

---
+++ ArraysAndNested Arrays-Lab/BiggerHalf.js
@@ -0,0 +1,12 @@
+function solve(array){
+    array.sort((a,b) => a - b);
+    newArray =[];
+    for (let index = Math.floor(array.length/2); index < array.length; index++) {
+       
+        newArray.push(array[index])
+    }
+    
+    console.log(newArray);
+    }
+
+    solve([3, 19, 14, 7, 2, 19, 6])
