e16cc9b72e3097f0887fac1fb5ef1bab9b9ca053
Elena Popova <elipopovadev@gmail.com>
2021-08-19 23:22:37 +0200

    MagicMatrices-IterateThroughRowsAndCols

--- ArraysAndNested Arrays-Exercise/MagicMatrices.js
+++ ArraysAndNested Arrays-Exercise/MagicMatrices.js
@@ -1,28 +1,28 @@
-function solve(matrix){
+function solve(matrix) {
     let arrayForRows = [];
     let sumRow = 0;
     for (let row = 0; row < matrix.length; row++) {
         for (let col = 0; col < matrix[row].length; col++) {
-           sumRow+= matrix[row][col];          
+            sumRow += matrix[row][col];
         }
 
         arrayForRows.push(sumRow);
-        sumRow = 0;       
+        sumRow = 0;
     }
 
     let arrayForCols = [];
     let sumCol = 0;
     for (let col = 0; col < matrix[0].length; col++) {
-       for (let row = 0; row < matrix.length; row++) {
-          sumCol+= matrix[row][col];
-       }
+        for (let row = 0; row < matrix.length; row++) {
+            sumCol += matrix[row][col];
+        }
 
-       arrayForCols.push(sumCol);
+        arrayForCols.push(sumCol);
         sumCol = 0;
     }
 
     let concatArray = arrayForRows.concat(arrayForCols);
-    console.log(concatArray.every(x => x === concatArray[0])); 
+    console.log(concatArray.every(x => x === concatArray[0]));
 }
 
 solve ([[4, 5, 6],
