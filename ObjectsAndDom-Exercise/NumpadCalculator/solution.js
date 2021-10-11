f0ee902d40babe338b65d98bf29c3f98ae07bf49
Elena Popova <elipopovadev@gmail.com>
2021-10-11 13:03:55 +0200

    NumpadCalculator - querySelector

--- ObjectsAndDom-Exercise/NumpadCalculator/solution.js
+++ ObjectsAndDom-Exercise/NumpadCalculator/solution.js
@@ -1,49 +1,44 @@
 function solve() {
-    let numberButtons = document.querySelectorAll('[data-number]');
-    let operationButtons = document.querySelectorAll('[data-operation]');
-    let equalsButton = document.querySelector('[data-equals]');
-    let clearButton = document.querySelector('.clear');
+  let numberButtons = document.querySelectorAll('[data-number]');
+  let operationButtons = document.querySelectorAll('[data-operation]');
+  let equalsButton = document.querySelector('[data-equals]');
+  let clearButton = document.querySelector('.clear');
 
-    let expressionOutput = document.getElementById('expressionOutput');
-    let resultOutput = document.getElementById('resultOutput');
+  let expressionOutput = document.getElementById('expressionOutput');
+  let resultOutput = document.getElementById('resultOutput');
 
-
-    numberButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            expressionOutput.value += button.innerHTML;          
-        })
-    });
-
-    operationButtons.forEach(button => {
-        button.addEventListener('click', add => {
-            expressionOutput.value +=' ' + button.innerHTML + ' ';  
-            removeEventListener('click', add );              
-        })
-        
-    });
-
-    equalsButton.addEventListener('click', () => {
-         let currentExpressionOutput = expressionOutput.value.split(' ');
-         let result = '';
-         if(currentExpressionOutput[1]=== '+') {
-            result = parse.Int(currentExpressionOutput[0]) + parse.Int(currentExpressionOutput[2]);
-         } else if(currentExpressionOutput[1] === '-') {
-            result = parse.Int(currentExpressionOutput[0]) - parse.Int(currentExpressionOutput[2]);
-         } else if(currentExpressionOutput[1] === '*') {
-            result = parse.Int(currentExpressionOutput[0]) * parse.Int(currentExpressionOutput[2]);
-         } else {
-            result = parse.Int(currentExpressionOutput[0]) / parse.Int(currentExpressionOutput[2]);
-         }
-
-         currentExpressionOutput.value= '';
-         resultOutput.value = result;   
+  for (const button of numberButtons) {
+    button.addEventListener('click', () => {
+      expressionOutput.innerHTML += button.innerHTML;
     })
+  }
 
+  for (const button of operationButtons) {
+    button.addEventListener('click', () => {
+      expressionOutput.innerHTML += ` ${button.innerHTML} `;
+    }, { once: true });
+  }
 
-    clearButton.addEventListener('click', () => {
-        expressionOutput.value = '';
-        resultOutput.value = '';
-    })
+  equalsButton.addEventListener('click', () => {
+    let currentExpressionOutput = expressionOutput.innerHTML.split(' ');
+    let result = '';
+    if (currentExpressionOutput[1] === '+') {
+      result = parseFloat(currentExpressionOutput[0]) + parseFloat(currentExpressionOutput[2]);
+    } else if (currentExpressionOutput[1] === '-') {
+      result = parseFloat(currentExpressionOutput[0]) - parseFloat(currentExpressionOutput[2]);
+    } else if (currentExpressionOutput[1] === 'x') {
+      result = parseFloat(currentExpressionOutput[0]) * parseFloat(currentExpressionOutput[2]);
+    } else {
+      result = parseFloat(currentExpressionOutput[0]) / parseFloat(currentExpressionOutput[2]);
+    }
 
+    currentExpressionOutput.innerHTML = '';
+    resultOutput.innerHTML = result;
+  })
+
+  clearButton.addEventListener('click', () => {
+    expressionOutput.innerHTML = '';
+    resultOutput.innerHTML = '';
+  })
 }
 
