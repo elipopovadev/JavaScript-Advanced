b8eaf6c74a21940059bcfd564522f5a80d92e8c1
Elena Popova <elipopovadev@gmail.com>
2021-10-20 22:27:20 +0200

    Shopping-Cart

---
+++ ObjectsAndDom-Exercise/Shopping-Cart/solution.js
@@ -0,0 +1,37 @@
+function solve() {
+    let allbuttons = document.getElementsByTagName('button');
+    let textarea = document.getElementsByTagName('textarea')[0];
+    let totalPrice = 0;
+    let allOrderedProducts = [];
+
+    for (const button of allbuttons) {
+        button.addEventListener('click', () => {
+            if (button.className === 'checkout') {
+                selectedCheckout();
+                return;
+            }
+
+            addProductToCart(button);
+        })  
+    }
+
+    function selectedCheckout() {
+        let productsList = allOrderedProducts.join(', ');
+        textarea.innerHTML += `You bought ${productsList} for ${totalPrice.toFixed(2)}.`;
+        document.querySelectorAll('button.className').forEach(elem => {
+            elem.disabled = true;
+        })
+    }
+
+    function addProductToCart(button) {
+        let parentProductAdd = button.parentElement;
+        let productDetails = parentProductAdd.previousElementSibling;
+        let productTitle = productDetails.firstElementChild.textContent;
+        let productPrice = parentProductAdd.nextElementSibling.textContent;
+        totalPrice += Number(productPrice);
+        textarea.innerHTML += `Added ${productTitle} for ${productPrice} to the cart.\n`;
+        if (!allOrderedProducts.includes(productTitle)) {
+            allOrderedProducts.push(productTitle);
+        }
+    }
+}
